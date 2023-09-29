3114f2fa97ee3d511e214aed0acaed229109c760
Lucia <lposadagonzalez@danielcastelao.org>
2023-09-29 09:39:17 +0200

    Añadido main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hurricane Walaka was the nineteenth named storm and second Category 5 hurricane of the 2018 Pacific hurricane season");
+    }
+}
