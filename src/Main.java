847ae99c2b8628c0f09676470a9e2c9aee2b9979
Lucia <lposadagonzalez@danielcastelao.org>
2023-09-29 09:42:20 +0200

    Añadido Titulo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hurricane Walaka was the nineteenth named storm and second Category 5 hurricane of the 2018 Pacific hurricane season");
+        System.out.println("Hurricane Walaka");
+        System.out.println("Hurricane Walaka was the nineteenth named storm and second Category 5 hurricane of the 2018 Pacific hurricane season");
     }
 }
